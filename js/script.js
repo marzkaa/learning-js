7ac95321f093f28d15b5eb6b4a5e4dad8bfc143d
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-02-18 22:22:59 +0100

    Add content to script.js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var data = [
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
+];
+
+for (var i=0; i < data.length; i++) {
+    var box = document.createElement('div');
+    box.id = data[i].id
+    box.innerHTML = '<h1>' + data[i].title + '</h1>' + data[i].content;
+    box.className = data[i].categories.join(' ');
+    document.body.appendChild(box)
+} 
