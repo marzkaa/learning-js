4643817f211a4a5c799838b36fb08a3f5f366c92
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-02-25 22:17:57 +0100

    Add changes

--- js/script.js
+++ js/script.js
@@ -4,33 +4,42 @@ var modalLinks = document.querySelectorAll('.show-modal');
 var closeButtons = document.querySelectorAll('.modal .close');
 var modalOverlay = document.querySelector('#modal-overlay');
 
-(function(){ 
-		
-	function showModal(event){
+
+
+(function modal(){ 
+
+    initialize();
+
+    function initialize() {
+        registerListeners();
+    };	
+
+	var showModal = function(event){
 		event.preventDefault();
 	    for (var i = 0; i < modals.length; i++) { 
 		    modals[i].classList.remove('show');
 	    }
 	    document.querySelector(this.getAttribute('href')).classList.add('show');
 	    modalOverlay.classList.add('show');	
-	};
+    };
     
-	for (var i = 0; i < modalLinks.length; i++) {
-        for (var j = 0; j < closeButtons.length; j++) {
-            for (var k = 0; k < modals.length; k++) {
-            modalOverlay.addEventListener('click', hideModal);
-            modalLinks[i].addEventListener('click', showModal);
-            closeButtons[j].addEventListener('click', hideModal);
-            modals[k].addEventListener('click', function(event){
-                event.stopPropagation();
-                });
-            }
-        }  
-    }
-
-	function hideModal(event) {
+    var hideModal = function(event) {
 		event.preventDefault();
 		modalOverlay.classList.remove('show');
     };
-	
-})(); 
+    
+    function registerListeners() {
+        for (var i = 0; i < modalLinks.length; i++) {
+            modalLinks[i].addEventListener('click', showModal);
+        }
+        for (var i = 0; i < closeButtons.length; i++) {
+            closeButtons[i].addEventListener('click', hideModal);
+        }
+        for (var i = 0; i < modals.length; i++) {
+            modals[i].addEventListener('click', function(event){
+                event.stopPropagation();
+            });
+        }
+        modalOverlay.addEventListener('click', hideModal);
+    };	
+})();
