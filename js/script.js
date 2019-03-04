dd064b7e71780a9dc4a646bc09ceea22c8118f48
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-03-04 18:40:34 +0100

    Add content to script

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var elem = document.querySelector('.main-carousel');
+var progressBar = document.querySelector('.progress-bar');
+var btnRestart = document.querySelector('.btn-restart');
+var templateSlide = document.getElementById('template-carousel').innerHTML;
+var carousel = document.querySelector('.main-carousel');
+var renderedTemplates = '';
+
+//mustache template
+Mustache.parse(templateSlide);
+for (var i = 0; i < data.length; i++) {
+  renderedTemplates += Mustache.render(templateSlide, data[i]);
+}
+carousel.innerHTML = renderedTemplates;
+
+// flickity
+var flkty = new Flickity( elem, {
+  cellAlign: 'left',
+  contain: true,
+  hash: true,
+  pageDots: false,
+});
+
+// button restart
+btnRestart.addEventListener('click', function(){
+  flkty.select(0);
+});
+
+ // scroll
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+
+// google maps
+window.initMap = function() { 
+  var map = new google.maps.Map(document.getElementById('map'), {    
+    center: data[0].coords,
+    zoom: 3                
+  }); 
+  for (var i = 0; i < data.length; i++) {
+    var marker = new google.maps.Marker({
+      position: data[i].coords,
+      map: map
+    });
+  }
+};
