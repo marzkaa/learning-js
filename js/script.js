76b4f232fb5f53e2934dead5423271295e6adad3
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-03-06 21:35:04 +0100

    Add script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+(function() {
 
+    var accordionJQuery = function() {
+        $('#accordion-jquery h3').click(function() {
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+    };
+
+    accordionJQuery();
+
+})(); 
