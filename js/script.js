1595a0047fbabd69be737e3867a93a61417e2b11
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-03-27 20:46:13 +0100

    Move getWarrantyCost to prototyp

--- js/script.js
+++ js/script.js
@@ -8,15 +8,16 @@ function Phone(brand, price, color, storage, premiere) {
     this.color = color;
     this.storage = storage;
     this.premiere = premiere;
-    getWarrantyCost = function() {
-		return " , the warranty cost is" + this.price * 0.1;
-	}
+    
 
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + getWarrantyCost() + ", storage is " + this.storage + " and the premiere was in " + this.premiere + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + " , the warranty cost is " + this.getWarrantyCost() + ", storage is " + this.storage + " and the premiere was in " + this.premiere + ".");
 }
 
+Phone.prototype.getWarrantyCost = function() {
+    return  this.price * 0.1;
+}
 
 iPhone6S.printInfo();
 SamsungGalaxyS6.printInfo();
