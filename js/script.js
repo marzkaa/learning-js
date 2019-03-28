43139b40315b0e0599111870b30ba7e4c1d16cab
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-03-28 22:38:55 +0100

    Add table

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = document.getElementById('countries');
+var countries = document.getElementById('countries');
 
 document.getElementById('search').addEventListener('click', searchCountries);
 
@@ -8,18 +8,30 @@ function searchCountries() {
     if(!countryName.length) countryName = 'Poland';
     fetch(url + countryName)
         .then(function(resp) {
+            document.querySelector('table').classList.add('show');
             return resp.json();
         })
         .then(showCountriesList);
 };
 
 function showCountriesList(resp) {
-    countriesList.innerHTML = '';
+    countries.innerHTML = '';
     resp.forEach(function(item) {
-        resp.forEach(function(item){
-            var liEl = document.createElement('li');
-            liEl.innerText = item.name;
-            countriesList.appendChild(liEl);
+            var newRow = document.createElement('tr');
+            var countryName = document.createElement('td');
+            var countryCapital = document.createElement('td');
+            var countryRegion = document.createElement('td');
+            var countryPopulation = document.createElement('td');
+            
+            countryName.innerText = item.name;
+            countryCapital.innerText = item.capital;
+            countryRegion.innerText = item.region;
+            countryPopulation.innerText = item.population;
+            
+            countries.appendChild(newRow);
+            newRow.appendChild(countryName);
+            newRow.appendChild(countryCapital);
+            newRow.appendChild(countryRegion);
+            newRow.appendChild(countryPopulation);
         });
-    });
 }
