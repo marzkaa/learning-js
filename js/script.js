ff7d759b39c60762e34209e9a0be492339cb8ecb
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-04-14 18:27:07 +0200

    add counters

--- js/script.js
+++ js/script.js
@@ -1,2 +1,61 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+var Counter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            default: 0
+        }
+    },
+    
+    getInitialState: function() {
+        return {
+            counter: this.props.default,
+        };
+    },
+    
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter -1
+        });
+    },
+
+    componentWillMount: function() {
+        console.log('komponent przed przerenderowaniem');
+    },
+    componentWillReceiveProps: function() {
+        console.log('komponent otrzymał nowe właściwości');
+    },
+    shouldComponentUpdate: function() {
+        console.log('Sprawdzenie optymalizacji komponentu');
+        return true;
+    },
+    componentWillUpdate: function() {
+        console.log('Komponent został zoptymalizowany');
+    },
+
+    render: function(){
+        console.log("ładowanie komponentu");
+        return React.createElement('div', {},
+            React.createElement('span', {}, ' Licznik ' + this.state.counter + ' '),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
+        );
+    },
+    componentDidMount: function() {
+        console.log('komponent zamontowany w drzewie DOM');
+    },
+    componentDidUpdate: function() {
+        console.log('komponent zamontowany w drzewie DOM po optymalizacji');
+    },
+});
+
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+var elementTwo = React.createElement(Counter);
+ReactDOM.render(elementTwo, document.getElementById('appTwo'));
