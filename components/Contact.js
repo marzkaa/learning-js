8d9e472d306d95911fddf7697dac5302b3469e7d
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-04-15 19:30:19 +0200

    add contact form

---
+++ components/Contact.js
@@ -0,0 +1,24 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        <div className= {'contactItem'}>
+            <img className={'contactImage'} src={'https://img.icons8.com/windows/104/000000/contacts.png'}/>
+            <div>
+                <p className= {'contactLabel'}>
+                Imię: {this.props.item.firstName}
+                </p>
+                <p className= {'contactLabel'}>
+                Imię: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                {this.props.item.email}
+                </a>
+            </div>
+        </div>
+      )
+    },
+  });
