83e24ae38cdc29e0b0a297e0d791c6e94e548a86
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-04-16 23:06:07 +0200

    add 3. exercise

--- js/script.js
+++ js/script.js
@@ -4,21 +4,28 @@ const x = 'Hello';
 const y = 'World';
 console.log(`${x} ${y}`);
 
-const helloWorld = (x ='Hello', y ='World' ) => console.log(`${x} ${y}`);
+const helloWorld = (h ='Hello', w ='World' ) => console.log(`${h} ${w}`);
 
 //Zadanie 2
 
-const multiply = (x, y = 1) => x * y;
+const multiply = (x = 1, y = 1) => console.log(`${x * y}`);
 multiply(2, 5);
 multiply(6, 6);
 multiply(5);
 
 //Zadanie 3
+const average = arr => arr.reduce((prev, next) => prev + next)/ arr.length;
+const arr = [1 ,2 ,3]
+console.log(average(arr));
+/* console.log(average(1));
+average(1, 3);
+average(1, 3, 6, 6); */
+
 //Zadanie 4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
 
 //Zadanie 5
 
-const array = [1, 4, 'Iwona', false, 'Nowak'];
-const [, , firstName, , lastName] = array;
-console.log(`${firstname} ${lastname}`);
+const array = [ 1, 4, 'Iwona', false, 'Nowak'];
+[, , firstname, , lastname] = array;
+console.log(firstname, lastname);
--- script-compiled.js
+++ script-compiled.js
@@ -1,14 +1,47 @@
+'use strict';
+
 //Zadanie 1
 
-const x = 'Hello';
-const y = 'World';
-console.log(`${x} ${y}`);
+var x = 'Hello';
+var y = 'World';
+console.log(x + ' ' + y);
 
-const helloWorld = (x = 'Hello', y = 'World') => console.log(`${x} ${y}`);
+var helloWorld = function helloWorld() {
+  var h = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Hello';
+  var w = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'World';
+  return console.log(h + ' ' + w);
+};
 
 //Zadanie 2
 
-const multiply = (x, y = 1) => x * y;
-multiply(x, y);
+var multiply = function multiply() {
+  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return console.log('' + x * y);
+};
+multiply(2, 5);
+multiply(6, 6);
+multiply(5);
 
 //Zadanie 3
+var average = function average(arr) {
+  return arr.reduce(function (prev, next) {
+    return prev + next;
+  }) / arr.length;
+};
+var arr = [1, 2, 3];
+console.log(average(arr));
+/* console.log(average(1));
+average(1, 3);
+average(1, 3, 6, 6); */
+
+//Zadanie 4
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+//Zadanie 5
+
+var array = [1, 4, 'Iwona', false, 'Nowak'];
+firstname = array[2];
+lastname = array[4];
+
+console.log(firstname, lastname);
