2637843734d3538ffd115f89fbad56c4494b2b12
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-04-16 22:11:02 +0200

    3 exercises

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+//Zadanie 1
+
+const x = 'Hello';
+const y = 'World';
+console.log(`${x} ${y}`);
+
+const helloWorld = (x ='Hello', y ='World' ) => console.log(`${x} ${y}`);
+
+//Zadanie 2
+
+const multiply = (x, y = 1) => x * y;
+multiply(2, 5);
+multiply(6, 6);
+multiply(5);
+
+//Zadanie 3
+//Zadanie 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+
+//Zadanie 5
+
+const array = [1, 4, 'Iwona', false, 'Nowak'];
+const [, , firstName, , lastName] = array;
+console.log(`${firstname} ${lastname}`);
---
+++ script-compiled.js
@@ -0,0 +1,14 @@
+//Zadanie 1
+
+const x = 'Hello';
+const y = 'World';
+console.log(`${x} ${y}`);
+
+const helloWorld = (x = 'Hello', y = 'World') => console.log(`${x} ${y}`);
+
+//Zadanie 2
+
+const multiply = (x, y = 1) => x * y;
+multiply(x, y);
+
+//Zadanie 3
