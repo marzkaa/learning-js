ac3a2922243a944d2095657a94a4edf2b6687d35
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-05-01 23:18:01 +0200

    Add plugins and change TodoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from './Todo';
+import style from './TodoList.css';
+
+
+const TodoList = props => {
+	const taskList = props.taskList.map(item =>
+		<Todo item={item} remove={props.remove} />
+	);
+	return <ul className={style.TodoList} >{taskList}</ul>
+}
+
+export default TodoList;    
