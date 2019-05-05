870e8c9eb910ba572cb09d9d9f99ea96265f9206
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-05-05 17:32:14 +0200

    Add middleware

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+var port = 9000;
+app.listen(port, function(){
+  console.log('listening on ' + port + '...')
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
