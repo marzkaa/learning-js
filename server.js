cd3c26cd2bc878c7a856a9c25b55b5fdff3f5c06
Marzena Smigiel <marzena.smigiel@gmail.com>
2019-05-06 19:21:59 +0200

    Add two middleware

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ app.use('/store', function(req, res, next){
     next();
 });
 
+app.use('/user', function (req, res, next) {
+    console.log('Autoryzacja użytkownika...')
+    next()
+  }, function (req, res, next) {
+    console.log('Sprawdzanie uprawnień')
+    next()
+  })
+
 app.get('/', function (req, res) {
     res.send('Hello world!');
 });
 
 app.get('/store', function (req, res) {
     res.send('To jest sklep');
+});
+
+app.get('/user', function (req, res) {
+    res.send('Pomyślna autoryzacja użytkownika');
 });
 
 var port = 9000;
