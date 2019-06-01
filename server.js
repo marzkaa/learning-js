52736f2cf18b994ab5db451be242fb8166af7bd3
Marzena <marzena.smigiel@gmail.com>
2019-06-01 10:58:53 +0200

    Correct password

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://marzena:Skeleton1%21@cluster0-8oox5.mongodb.net/test?retryWrites=true&w=majority',{ 
+mongoose.connect('mongodb+srv://kondrad:test@cluster0-8oox5.mongodb.net/test?retryWrites=true&w=majority',{ 
     useNewUrlParser: true 
 });
 
